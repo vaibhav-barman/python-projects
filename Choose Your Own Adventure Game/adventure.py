096074dea77b9b05ca6f9444f66b087ab653f68f
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-14 20:38:17 +0530

    Add Choose Your Own Adventure Game

--- Choose Your Own Adventure Game/adventure.py
+++ Choose Your Own Adventure Game/adventure.py
@@ -20,3 +20,35 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_______/
 *******************************************************************************
 ''')
+
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go (left/right)? ").lower()
+
+if answer=="left":
+    answer = input("You come to a river, you can walk around it or swim across? Type 'walk' to walk around or 'swim' to swim across: ").lower()
+    if answer=="swim":
+        print("You swam across and were eaten by a Caiman. You Lose!")
+    elif answer=="walk":
+        print("You ran for many miles, ran out of water and you lost the game.")
+    else:
+        print("Invalid Choice. You Lose!")
+elif answer=="right":
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ").lower()
+    if answer=="back":
+        print("You go back and you lose!")
+    elif answer=="cross":
+        answer = input("You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ").lower()
+        if answer=="yes":
+            print("You talk to the stranger and they give you gold. You Win!!")
+        elif answer=="no":
+            print("You ignored stranger and they stab you in the bacl. You Lose!")
+        else: 
+            print("Invalid Choice. You Lose!")
+    else:
+        print("Invalid Choice. You Lose!")
+else:
+    print("Invalid Choice. You Lose!")
+
+print("Thanks for playig the game,", name + "!")
