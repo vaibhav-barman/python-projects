7f6c322ef6a999d534e47a1e49ea0f0276d0c729
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-15 23:25:52 +0530

    feat: generate encryption key

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -2,6 +2,13 @@ from cryptography.fernet import Fernet
 
 master_pwd = input("What is the master password? ")
 
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+        
 def view():
     with open("passwords.txt", "r") as f:
             for line in f.readlines():
