10c16daf3cac3c08843ae697dcfaea2779d247cf
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-15 23:16:16 +0530

    feat: add password viewing functionality

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -1,7 +1,12 @@
 master_pwd = input("What is the master password? ")
 
 def view():
-    pass
+    with open("passwords.txt", "r") as f:
+            for line in f.readlines():
+                data = line.rstrip()
+                user, passw = data.split("|")
+                print("User:", user, "Password:", passw)
+
 
 def add():
     name = input("Account Name: ")
