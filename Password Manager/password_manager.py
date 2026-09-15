bbf280c9d86051a04ebbb127df9eb14d2370419d
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-15 23:45:56 +0530

    refactor: simplify password manager encryption

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -13,8 +13,7 @@ def load_key():
     file.close()
     return key
 
-master_pwd = input("What is the master password? ")
-key = load_key() + master_pwd.encode()
+key = load_key()
 fer = Fernet(key)
 
 def view():
@@ -22,7 +21,7 @@ def view():
             for line in f.readlines():
                 data = line.rstrip()
                 user, passw = data.split("|")
-                print("User:", user, "Password:", fer.decrypt(passw.encode()))
+                print("User:", user, "Password:", fer.decrypt(passw.encode()).decode())
 
 
 def add():
