f2f8d01f470cd176020f057f3da851bc8f2feae0
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-15 23:35:06 +0530

    feat: encrypt saved passwords

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -1,7 +1,5 @@
 from cryptography.fernet import Fernet
 
-master_pwd = input("What is the master password? ")
-
 '''
 def write_key():
     key = Fernet.generate_key()
@@ -14,6 +12,10 @@ def load_key():
     key = file.read()
     file.close()
     return key
+
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
     with open("passwords.txt", "r") as f:
@@ -28,7 +30,7 @@ def add():
     pwd = input("Password: ")
 
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view an existing one (view/add), press q to quit? ").lower()
