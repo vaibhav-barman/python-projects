b9d7157edf6d0b186e218c5e141822b671e9ef43
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-15 23:40:51 +0530

    fix: handle encrypted password string format

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -22,7 +22,7 @@ def view():
             for line in f.readlines():
                 data = line.rstrip()
                 user, passw = data.split("|")
-                print("User:", user, "Password:", str(fer.decrypt(passw.encode())))
+                print("User:", user, "Password:", fer.decrypt(passw.encode()))
 
 
 def add():
@@ -30,7 +30,7 @@ def add():
     pwd = input("Password: ")
 
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view an existing one (view/add), press q to quit? ").lower()
