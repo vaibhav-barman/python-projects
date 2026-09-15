a6e68aae0d077db7335504a3b23fbd9429518cfd
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-15 23:36:38 +0530

    feat: decrypt passwords when viewing

--- Password Manager/password_manager.py
+++ Password Manager/password_manager.py
@@ -22,7 +22,7 @@ def view():
             for line in f.readlines():
                 data = line.rstrip()
                 user, passw = data.split("|")
-                print("User:", user, "Password:", passw)
+                print("User:", user, "Password:", str(fer.decrypt(passw.encode())))
 
 
 def add():
